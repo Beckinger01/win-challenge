b3db54b1f548e92a3be36faacc792b8bfad6e987
Niklas <niklasbeck18@gmail.com>
2025-04-13 16:43:08 +0200

    port changes

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const next = require('next');
 const { Server } = require('socket.io');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
-const app = next({ dev, hostname, port });
+const port = process.env.PORT || 3000;
+
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -41,6 +41,6 @@ app.prepare().then(() => {
   global.io = io;
 
   server.listen(port, () => {
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log(`> Ready on port ${port}`);
   });
 });

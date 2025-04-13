4af09f4d51c8a7de5c078934473ddff2e9947207
Niklas <niklasbeck18@gmail.com>
2025-04-13 16:50:16 +0200

    adsense update

--- components/GoogleAnalytics.jsx
+++ components/GoogleAnalytics.jsx
@@ -4,30 +4,34 @@ import { useEffect } from "react";
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
-    useEffect(() => {
-        // Check if user has consented to cookies
-        const cookieConsent = localStorage.getItem("cookieConsent");
+  useEffect(() => {
+    // Check if user has consented to cookies
+    const cookieConsent = localStorage.getItem("cookieConsent");
 
-        // If we have the Google Analytics tags loaded but user has only consented to essential cookies,
-        // we should respect this and disable tracking
-        if (cookieConsent === "essential" && window.gtag) {
-            window.gtag('consent', 'update', {
-                'analytics_storage': 'denied'
-            });
-        }
-    }, []);
+    // If we have the Google Analytics tags loaded but user has only consented to essential cookies,
+    // we should respect this and disable tracking
+    if (cookieConsent === "essential" && window.gtag) {
+      window.gtag('consent', 'update', {
+        'analytics_storage': 'denied'
+      });
+    }
+  }, []);
 
-    return (
-        <>
-            {/* Load the Google Analytics script */}
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-V4JSECSYVT"
-                strategy="afterInteractive"
-            />
+  return (
+    <>
 
-            {/* Initialize Google Analytics with consent mode */}
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1511082465942403"
+        crossorigin="anonymous"></script>
+        
+      {/* Load the Google Analytics script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-V4JSECSYVT"
+        strategy="afterInteractive"
+      />
+
+      {/* Initialize Google Analytics with consent mode */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -50,7 +54,7 @@ export default function GoogleAnalytics() {
             });
           }
         `}
-            </Script>
-        </>
-    );
+      </Script>
+    </>
+  );
 }

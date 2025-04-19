a0401c52d4cbad622e76e6bdf474461e3da52f46
Niklas <niklasbeck18@gmail.com>
2025-04-19 14:04:42 +0200

    adsense change

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import '@styles/globals.css';
 import Provider from '@components/Provider';
 import Nav from '@components/Nav';
 import GoogleAnalytics from '@components/GoogleAnalytics';
+import Script from 'next/script';
 
 export const metadata = {
   title: "Win-Challenge",
@@ -11,6 +12,14 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1511082465942403"
+          crossorigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         <Provider>
           <main className='pt-16 font-int'>
